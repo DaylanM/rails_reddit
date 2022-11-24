efbfe290ca60816e341ecc8fdd58695e73c72fa1
Daylan Merritt <daylan.merritt@gmail.com>
2022-11-23 19:01:17 -0700

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,52 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # http verb - how the browser communicates the crud actions
+  Create -> POST
+  Read -> GET
+  Update -> PUT / PATCH
+  Destroy -> DELETE
+
+  # rails this file does navigation
+  # rails with react, lead us somewhere in our controller
+  # table of content
+
+  # manually, custom function
+  # http verb, to: 'controller(s)#action'
+  # get '/subs/', to: 'subs#index'
+  # get '/subs/:id', to: 'subs#show'
+      # :id -> placeholder
+        # /subs/23
+  # post '/subs/', to: 'subs#create'
+  # put '/subs/', to: 'subs#update'
+  # delete '/subs/', to: 'subs#destroy'
+
+  # resources :controller(s)
+  resources :controller :subs, only: [:show, :create]
+  resources :controller :subs, except: [:index, :update]
+
+
+  namespace :api do
+    resouces :subs
+  end
+
+  # if there is another model and is a child of this model then the routes would be embedded routes
+
+  #   resouces :parent(s) do
+  #     resouces :child(s)
+  #   end
+
+  # only two levels deep, any more will be too complicated
+
+  ### don't do this
+  #   resouces :parent(s) do
+  #     resouces :child(s)
+  #       resources :grandchild(s)
+  #         resouces :grandgrandchild(s)
+  #   end
+
+
+
+
 end
