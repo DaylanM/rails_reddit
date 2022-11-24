bd56b215fa35eedf7df632adb3ce302f9b3963ca
Daylan Merritt <daylan.merritt@gmail.com>
2022-11-23 20:44:55 -0700

    added comment routes and controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,65 +4,72 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  # http verb - how the browser communicates the crud actions
+  # http verb - how the browser commuicates the crud action
   # Create -> POST
   # Read -> GET
   # Update -> PUT / PATCH
-  # Destroy -> DELETE
+  # Destroy -> DELETE 
 
   # rails this file does navigation
-  # rails with react, lead us somewhere in our controller
+  # rails with react, lead us somewhere in our controller 
   # table of content
 
-  # manually, custom function
-  # http verb, to: 'controller(s)#action'
-  # get '/subs/', to: 'subs#index'
+  # manually, custom function 
+  # http verb 'urlpath', to: 'controller(s)#action'
+  # get '/subs', to: 'subs#index'
+    # :id -> placeholder
+    # :parent_id
+      #  /subs/23
   # get '/subs/:id', to: 'subs#show'
-      # :id -> placeholder
-        # /subs/23
   # post '/subs/', to: 'subs#create'
-  # put '/subs/', to: 'subs#update'
-  # delete '/subs/', to: 'subs#destroy'
+  # put '/subs/:id', to: 'subs#update'
+  # delete '/subs/:id', to: 'subs#destroy'
 
   # resources :controller(s)
-  # resources :controller :subs, only: [:show, :create]
-  # resources :controller :subs, except: [:index, :update]
+  # resources :subs, only: [:show, :create]
+  # resources :subs, except: [:index, :update]
 
-
-  namespace :api do
+  namespace :api do 
     resources :subs do
       resources :topics
     end
+
+    resources :topics, except: [:index, :show, :create, :update, :destroy] do
+      resources :comments
+    end
+
+    # resources :comments, except: [:index, :show, :create, :update, :destroy] do
+    #   resources :likes
+    # end
+
   end
 
-  # if there is another model and is a child of this model then the routes would be embedded routes
+  # if there is a another model and is a child of this model then the
+  # routes be embedded routes
 
-  #   resources :parent(s) do
-  #     resources :child(s)
+  # resources :parent(s) do 
+  #   resources :child(s)
+  # end
+
+  # only two level deep, any more will be too hard
+
+  # * don't do this
+  # resources :parent(s) do 
+  #   resources :child(s) do 
+  #     resources :grandchild(s) 
+        #  resources :grandgrandchild
   #   end
+  # end
 
-  # only two levels deep, any more will be too complicated
+  # make things more complex
+  
+  # resources :parent(s) do 
+  #   resources :child(s)
+  # end
 
-  ### don't do this
-  #   resources :parent(s) do
-  #     resources :child(s)
-  #       resources :grandchild(s)
-  #         resources :grandgrandchild(s)
-  #   end
-
-
-  #   resources :parent(s) do
-  #     resources :child(s)
-  #   end
-
-  # this creates duplicate routes for the child
-  #   resources :child(s) except : [:index, :show, :create, :update, :destroy] do
-  #     resources :grandchild(s)
-  #   end
-
-
-
-
-
+  # this create duplicate routes for the child
+  # resources :child(s), except: [:index, :show, :create, :update, :destroy] do 
+  #   resources :grandchild(s)
+  # end
 
 end
